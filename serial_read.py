4cbd1372c07a804d04b18b89f9844f307f5f6265
Andreu Roca Grange <andreurocagrange@pop-os.localdomain>
2020-08-30 12:48:49 +0200

    Try-except for ACM0, graph modifications. header modifications.

--- serial_read.py
+++ serial_read.py
@@ -3,48 +3,63 @@ import time
 import os
 import csv
 import matplotlib.pyplot as plt
+import sys
 
-#first_line=['data','classification']
-ser=serial.Serial('/dev/ttyACM0',115200)
+dataset_file_name="dataset.csv"
+n_samples=12000 #analog samples from arduino lectures. Not dataset sampels
 
-#creating file and writing the two headers stored on "first_line". Opening a file as 'W' erases everything it had before so in case you want to add new rows, comment this 3 lines
+while True:
+     try:
+         ser=serial.Serial('/dev/ttyACM0',115200)
+         print("Device connected!\n\n")
+         break
+     except FileNotFoundError and serial.serialutil.SerialException:
+         print("No device conected")
+         print("Reconnecting...")
+         time.sleep(3)
 
-with open('test.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
+#Header of the dataset: [0,1,2...11999,classification]. Writing it if file doesn't exist.
+number_list=[] #used as header and plots
+for i in range(n_samples):
+    number_list.append(i)
 
-#loop that cycles all the gestures a n_rep amount of times for a period of n samples
+if os.path.isfile(dataset_file_name):
+    print ("Appending new entries to current dataset "+ dataset_file_name)
+else:
+    print("There isn't any dataset in the directory...\nCreating a fresh one...\n")
+    with open(dataset_file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(number_list+["classification"])
+    time.sleep(1)
 
 classification=1
-n_samples=12000
 
 while (classification<9):
     n_reps=0
-    print("Gesture number", classification)
-    while (n_reps<3): #number of repetitions
-        print("reading serial port acm0")
+    print("=================")
+    print("Gesture number: ", classification, "\n")
+    while (n_reps<3): #number of repetitions for each gesture.
+        print("reading serial port. Iteration n "+ str(n_reps))
         i=0
         data=[]
-        while (i<n_samples):  #reading loop
+        while (i<n_samples):
             n_data=ser.read()
             data+=n_data
             i+=1
-        print("stop reading")
-        print("plotting...")
-        plt.plot(data)
-        plt.ylabel('wave form')
-        plt.show(1)
+
+        #Matplotlib plotting. Untill you don't close the graph window
+        #it doesnt' continue.
+        plt.plot(number_list,data)
+        plt.ylabel('entry n '+str(n_reps))
+        plt.xlabel('samples')
+        plt.show()
         n_reps+=1
-        
         data+=[classification]
-        with open('test.csv', 'a+', newline='') as file:
+        with open(dataset_file_name, 'a+', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(data)
-            
-    print("Gesture done, next")
-    print("\n")
+
+    print("Gesture number ", classification, "done, next \n\n\n")
     classification+=1
 
-
-
-
-
+ser.close()
