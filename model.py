b55d5faf0316d5ff5944248a22d21e1191d03666
Andreu Roca Grange <andreurocagrange@pop-os.localdomain>
2020-10-04 15:46:00 +0200

    Classifier working very very good.

--- model.py
+++ model.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from scipy.fft import fft
+from sklearn.svm import SVC
 from joblib import dump, load
 import serial
-from sklearn.neighbors import KNeighborsClassifier
 from scipy.signal import find_peaks, peak_prominences
 from serial_read import serial_signal_read, wait_until_serial_port_is_available_and_connect
+import matplotlib.pyplot as plt
 
 def train_model(dataset_path,model_path,train_test_split_var=True, debug=True):
     '''
@@ -22,19 +22,15 @@ def train_model(dataset_path,model_path,train_test_split_var=True, debug=True):
     X=dataset.iloc[:,:-1]
     if debug:
         print ("1Dataset shape: ",dataset.shape)
-    X_fourier=fft(X)
-    X_fourier_abs=np.abs(X_fourier)
-    X_f=pd.DataFrame.from_records(X_fourier_abs)
-
     if train_test_split_var==True:
         X_train, X_test, y_train, y_test = train_test_split(X_f, y, test_size=0.30)
         if debug:
             print ("Train and test shape: ",X_train.shape, X_test.shape)
-        model=LogisticRegression(random_state=0, max_iter=10000, penalty='none', solver='sag', multi_class='multinomial').fit(X_train, y_train)
+        model=SVC(random_state=1, kernel='rbf', C=80, gamma=0.0001,probability=True).fit(X_train, y_train)
         if debug:
             print("Test model accuracy score: ",model.score(X_test, y_test))
     else:
-        model=LogisticRegression(random_state=0, max_iter=10000, penalty='none', solver='sag', multi_class='multinomial').fit(X_f,y)
+        model=SVC(random_state=1, kernel='rbf', C=80, gamma=0.0001,probability=True).fit(X,y)
     print("model traied succesfully")
     dump(model, model_path)
     return model
@@ -58,11 +54,16 @@ def predict(data, model):
     Returns the predictes class "gesture_done" and the confidence of this prediction being a float between (0.251-1].
     '''
     #done this way so the program doesn't call predict 2 times which takes more time.
+    probabilities=model.predict_proba(data)[0]
+    print (probabilities)
+    index=0
+    high=0
 
-    probabilities=model.predict_proba(data)
-    confidence=np.amax(probabilities)
-    gesture_done=np.where(confidence)
-    return gesture_done, confidence
+    for i in range(len(probabilities)):
+        if probabilities[i]>high:
+            high=probabilities[i]
+            index=i
+    return index+1, probabilities
 
 
 def evaluate_model_in_live(idle_treshold, num_of_evaluations,debug=True):
@@ -76,8 +77,8 @@ def evaluate_model_in_live(idle_treshold, num_of_evaluations,debug=True):
     - class with most probability "model.predict"
     - number of times the signal cross the threshod "threshold_times_crossed"
     '''
-    model = load('model1.joblib')
-    ser=wait_until_serial_port_is_available_and_connect
+    model = load('model.joblib')
+    ser=wait_until_serial_port_is_available_and_connect()
     while num_of_evaluations!=0:
         sample_value=ser.read()
         while sample_value[-1]<idle_treshold:
@@ -88,16 +89,14 @@ def evaluate_model_in_live(idle_treshold, num_of_evaluations,debug=True):
         data, threshold_times_crossed=serial_signal_read(ser)
         peaks=find_peaks_num(data)
         print("Go!")
-        evaluation_data_fft=fft(data)
-        evaluation_data_abs=np.abs(evaluation_data_fft)
-        class_pertenance_probabilities=model.predict_proba([data+[threshold_times_crossed]+[peaks]])
-        print("probability: ",class_pertenance_probabilities)
-        print ("class: ", model.predict([data+[threshold_times_crossed]+[len(peaks)]]))
-        print("Threshold: ", threshold_times_crossed, "Peak: ", len(peaks))
+        gesture_done,confidence=predict([data+[threshold_times_crossed]+[peaks]],model)
+        print("probability: ",confidence)
+        print ("class: ", gesture_done)
+        print("Threshold: ", threshold_times_crossed, "Peak: ", peaks)
         num_of_evaluations-=1
     print("exit evaluation")
 
 
 if __name__ == '__main__':
-    #model=train_model("dataset_new_features.csv", train_test_split_var=False)
+    model=train_model("dataset.csv","model.joblib", train_test_split_var=False)
     evaluate_model_in_live(2, 15, debug=False) #low treshold, value of 3 of the adc, 15 iterarions, and prints deactivated
